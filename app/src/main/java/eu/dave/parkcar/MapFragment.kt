24f0192b9ceeb270226828046bf79d3ce4d65af1
Davide Di Giovanni <davidedigio@yahoo.it>
2023-06-19 21:15:56 +0200

    Added maps in MapFragment

--- app/src/main/java/eu/dave/parkcar/MapFragment.kt
+++ app/src/main/java/eu/dave/parkcar/MapFragment.kt
@@ -5,17 +5,67 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.material.tabs.TabLayout
 
-class MapFragment : Fragment() {
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mapView: MapView
+    private lateinit var googleMap: GoogleMap
+    private lateinit var tabLayout: TabLayout
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_map, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_map, container, false)
+        mapView = rootView.findViewById(R.id.mapView)
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+        return rootView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        mapView.onResume()
+        tabLayout = requireActivity().findViewById(R.id.tabLayout)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.onDestroy()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        googleMap = map
+
+        googleMap.uiSettings.isZoomGesturesEnabled = true
+
+        googleMap.uiSettings.isScrollGesturesEnabled = true
+
+        val parkingLocation = LatLng(41.9028, 12.4964)
+        googleMap.addMarker(MarkerOptions().position(parkingLocation).title("Parcheggio"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(parkingLocation, 12f))
     }
 }
